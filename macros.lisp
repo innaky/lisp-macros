242c9c361878d6d2b5625d997a60e4db7bc086c8
Innaky <innaky@protonmail.com>
2020-06-08 23:45:33 -0400

    (nw) macro for using gensym.

--- macros.lisp
+++ macros.lisp
@@ -2,3 +2,10 @@
   `(do ()
        ((not ,test))
      ,@body))
+
+(defmacro for ((var start stop) &body body)
+  (let ((gstop (gensym)))
+    `(do ((,var ,start (1+ ,var))
+	  (,gstop ,stop))
+	 ((> ,var ,gstop))
+       ,@body)))
